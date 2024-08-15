62eb55e4e83d896f40d4844e4d753f8315fc37e5
Roboticstristan <tmbrewer@mcdonogh.org>
2024-08-15 16:05:48 -0400

    ep 3

---
+++ main.py
@@ -0,0 +1,16 @@
+import numpy as np
+inputs= [1,2,3,2.5]
+weights = [0.2,0.8,-0.5,1.0]
+bias = 2
+
+#output of the layer we are currnetly calculating
+
+# layerOutputs = [] 
+# for neuronW, neuronB in zip(weights,biases):
+#     neuronO = 0
+#     for neuronI, weight in zip(inputs, neuronW):
+#         neuronO += neuronI * weight
+#     neuronO += neuronB
+#     layerOutputs.append(neuronO)
+
+# print(layerOutputs)
